2669672929e196d9b383e7b335322ef922e52ba8
larissa360 <133892376+larissa360@users.noreply.github.com>
2024-06-27 09:42:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abaconteudo");
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date (2025,02,25);
+const tempoObjetivo2 = new Date (2025,05,25);
+const tempoObjetivo3 = new Date (2025,08,25);
+const tempoObjetivo4 = new Date (2025,10,25);
+let agora = new Date();
+let segundos 
+let minutos
+let horas
+let dias 
+segundos = (tempoObjetivo1 - agora)/1000;
+minutos = segundos/60;
+horas = minutos/60;
+dias = horas/24;
 
+segundos =  Math.floor(segundos);
+minutos =  Math.floor(minutos);
+horas =  Math.floor(horas);
+dias =  Math.floor(dias);
+segundos = segundos%60;
+minutos = minutos%60;
+horas = horas%24;
+
+function calculaTempo(X,Y){
+    const agora = new Date()
+    let segundos
+    let minutos
+    let horas
+    let dias
+    segundos = (tempoObjetivo1 - agora)/1000;
+minutos = segundos/60;
+horas = minutos/60;
+dias = horas/24;
+
+segundos =  Math.floor(segundos);
+minutos =  Math.floor(minutos);
+horas =  Math.floor(horas);
+dias =  Math.floor(dias);
+segundos = segundos%60;
+minutos = minutos%60;
+horas = horas%24;
+}
+
+contador[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos.`
+contador[1].textContent = tempoObjetivo2 - agora
+contador[2].textContent = tempoObjetivo3 - agora
+contador[3].textContent = tempoObjetivo4 - agora
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
@@ -15,4 +61,3 @@ for (let i = 0; i < botoes.length; i++){
 
     }
 }
-
